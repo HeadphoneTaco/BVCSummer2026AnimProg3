5f38a393a1060ab537fc22363b62953b44165100
Michael Hardwicke <m.hardwicke596@mybvc.ca>
2026-08-10 17:51:46 -0600

    Start basic character controller

---
+++ UnityProjects/AA3/Assets/_Project/Code/PlayerController.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+ 
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float movementSpeed;
+    [SerializeField] float rotationSpeed;
+    [SerializeField] float runningSpeedMulitplier;
+ 
+    [SerializeField] Transform cameraTransform;
+ 
+    [SerializeField] InputActionReference moveInputAction;
+    [SerializeField] InputActionReference runInputAction;
+ 
+    Vector2 moveInput;
+ 
+    Rigidbody rb;
+ 
+    //Animator anim;
+ 
+    //float activeRunningSpeedMultiplier = 1f;
+ 
+    //readonly int walkingAnimatorHash = Animator.StringToHash("Walking");
+    //readonly int runningAnimatorHash = Animator.StringToHash("Running");
+ 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //anim = GetComponent<Animator>();
+    }
+ 
+    void Update()
+    {
+       moveInput = moveInputAction.action.ReadValue<Vector2>();
+    }
+ 
+    private void FixedUpdate()
+    {
+        Vector3 cameraForward = cameraTransform.forward;
+        Vector3 cameraRight = cameraTransform.right;
+ 
+        cameraForward.y = 0;
+        cameraRight.y = 0;
+ 
+        cameraForward.Normalize();
+        cameraRight.Normalize();
+ 
+        Vector3 moveDirection = cameraForward * moveInput.y + cameraRight * moveInput.x;
+ 
+        Vector3 velocity = moveDirection * movementSpeed;
+ 
+       rb.linearVelocity = new Vector3(velocity.x, rb.linearVelocity.y, velocity.z);
+ 
+       if (moveDirection == Vector3.zero)
+        {
+            //anim.SetBool(walkingAnimatorHash, false);
+            //anim.SetBool(runningAnimatorHash, false);
+           return;
+        }
+ 
+       // anim.SetBool(walkingAnimatorHash, true);
+ 
+        //if (runInputAction.action.IsPressed())
+        {
+            //anim.SetBool(runningAnimatorHash, true);
+            //activeRunningSpeedMultiplier = runningSpeedMulitplier;
+        }
+        //else
+        {
+           // anim.SetBool(runningAnimatorHash, false);
+           // activeRunningSpeedMultiplier = 1;
+        }
+ 
+       Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+ 
+       Quaternion finalRotation = Quaternion.Slerp(rb.rotation, targetRotation, rotationSpeed);
+ 
+       rb.MoveRotation(finalRotation);
+    }
+}
